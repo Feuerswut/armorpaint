a3ea3ec5d12f71cf920a7c42e9292e156bc28373
luboslenco <lubos.lenco@gmail.com>
2026-05-22 11:52:55 +0200

    macos: bump deployment target

--- base/tools/make.js
+++ base/tools/make.js
@@ -1539,7 +1539,7 @@ class XCodeExporter extends Exporter {
 			this.p('IPHONEOS_DEPLOYMENT_TARGET = 18.0;', 4);
 		}
 		else {
-			this.p('MACOSX_DEPLOYMENT_TARGET = 13.0;', 4);
+			this.p('MACOSX_DEPLOYMENT_TARGET = 14.0;', 4);
 		}
 		this.p('MTL_ENABLE_DEBUG_INFO = YES;', 4);
 		this.p('ONLY_ACTIVE_ARCH = YES;', 4);
